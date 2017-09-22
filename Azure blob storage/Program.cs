8bc2dc664e7e5f4d14bd13b47a5ac61c760e85e2
Siddhant Gosavi <siddhant.gosavi@rediffmail.com>
2017-09-22 17:46:08 +0530

    created multiple methods

--- Azure blob storage/Program.cs
+++ Azure blob storage/Program.cs
@@ -7,49 +7,112 @@ using Microsoft.WindowsAzure; // Namespace for CloudConfigurationManager
 using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
 using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
 using Microsoft.Azure; //Namespace for CloudConfigurationManager
+using Microsoft.Azure.Management.Storage.Models;
 
 namespace Azure_blob_storage
 {
     class Program
     {
+        // Parse the connection string and return a reference to the storage account.
+
+        private static CloudStorageAccount _storageAccount;
+        //static
+        //{private static CloudStorageAccount _storageAccount;
+        //Console.WriteLine("constructor");
+        //    StorageAccount = CloudStorageAccount.Parse(
+        //            CloudConfigurationManager.GetSetting("StorageConnectionStringTest"));
+        //}
+
         static void Main(string[] args)
         {
+            _storageAccount = CloudStorageAccount.Parse(
+                    CloudConfigurationManager.GetSetting("StorageConnectionStringTest"));
+
             try
             {
-                Console.WriteLine("Parsing storage account key");
-                // Parse the connection string and return a reference to the storage account.
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                    CloudConfigurationManager.GetSetting("StorageConnectionStringSid"));
-
-                Console.WriteLine("Creating blob client");
-                //Creating a Cloud Blob Client
-                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-                //This is the url to access blob in browser.
-                //https://sidstoragetrial1.blob.core.windows.net/first-container
-                //In .Net Code u ll require a connection string
-
-                // Retrieve a reference to a container.
-                CloudBlobContainer container = blobClient.GetContainerReference("testcontainerfromcode1");
-
-                Console.WriteLine("Container reference created \n creating container");
-
-                // Create the container if it doesn't already exist.
-                container.CreateIfNotExists();
-                
-                Console.WriteLine("created container");
-
-
-
-                container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
-
-                Console.WriteLine("public permission given");
-
+                CreateNewContainer("secondontainerfromcode");
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ContainerNameToBeCreated"></param>
+        private static void CreateNewContainer(String ContainerNameToBeCreated)
+        {
+            Boolean Status;
+
+            Console.WriteLine("Creating blob client");
+            //Creating a Cloud Blob Client
+            CloudBlobClient blobClient = _storageAccount.CreateCloudBlobClient();
+            Console.WriteLine("fatla");
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference(ContainerNameToBeCreated);
+            Console.WriteLine(ContainerNameToBeCreated);
+            Console.WriteLine("Container reference created \n creating container");
+
+            // Create the container if it doesn't already exist.
+            Status = container.CreateIfNotExists();
+
+            Console.WriteLine("container created");
+
+            container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            Console.WriteLine("public permission given");
+
+            //return Status;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private static IEnumerable<String> GetAllContainers()
+        {
+
+
+            return new List<String>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ContainerName"></param>
+        /// <returns></returns>
+        private static IEnumerable<Byte[]> GetAllBlobsFromContainer(String ContainerName)
+        {
+
+
+            return new List<Byte[]>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Filepath"></param>
+        /// <returns></returns>
+        private static Boolean InsertBlobInContainer(String Filepath)
+        {
+
+
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Folderpath"></param>
+        /// <returns></returns>
+        private static Boolean InsertBolbsFromFolderToContainer(String Folderpath)
+        {
+
+
+            return true;
         }
     }
 }
